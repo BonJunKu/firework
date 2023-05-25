7e61cf3bf68f782e134cecbc09ab767e0047343f
Bonjun Ku <ku96kbj@naver.com>
2023-05-26 00:41:13 +0900

    Fix Options

--- src/index.js
+++ src/index.js
@@ -2,6 +2,52 @@ import { Fireworks } from 'fireworks-js';
 
 const container = document.querySelector('.container');
 const fireworks = new Fireworks(container, {
-  /* options */
+  autoresize: false,
+  opacity: 0.5,
+  acceleration: 1.05,
+  friction: 0.97,
+  gravity: 1.5,
+  particles: 50,
+  traceLength: 3,
+  traceSpeed: 10,
+  explosion: 8,
+  intensity: 30,
+  flickering: 50,
+  lineStyle: 'round',
+  hue: {
+    min: 0,
+    max: 360,
+  },
+  delay: {
+    min: 90,
+    max: 100,
+  },
+  rocketsPoint: {
+    min: 50,
+    max: 50,
+  },
+  lineWidth: {
+    explosion: {
+      min: 1,
+      max: 3,
+    },
+    trace: {
+      min: 1,
+      max: 2,
+    },
+  },
+  brightness: {
+    min: 50,
+    max: 80,
+  },
+  decay: {
+    min: 0.015,
+    max: 0.03,
+  },
+  mouse: {
+    click: true,
+    move: false,
+    max: 1,
+  },
 });
 fireworks.start();
